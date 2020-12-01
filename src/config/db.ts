a8aebaec6cfaf5f131a4a455f7e7166398804a22
Juan Barrios <juanbarrios045@gmail.com>
2020-12-01 12:45:22 -0400

    Master - Change URI Mongo

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,7 @@
 import mongoose, { ConnectionOptions } from 'mongoose';
-import config from './index';
+import { config } from 'dotenv';
+
+config();
 
 (async () => {
   try {
@@ -7,14 +9,8 @@ import config from './index';
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
-      authSource: 'admin',
-      user: config.MONGO_USER,
-      pass: config.MONGO_PASSWORD,
     };
-    await mongoose.connect(
-      `mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`,
-      options
-    );
+    await mongoose.connect(process.env.MONGO_URI || '', options);
     console.log('DB is connected');
   } catch (e) {
     console.log('Error DB ->', e);
